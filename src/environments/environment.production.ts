bd8f53ee11b82ea5395f0b190f923d1ac11d9b3c
Cupcake <megatoncupcake@gmail.com>
2026-08-02 01:33:09 +0800

    fixed backend url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://ec2-13-210-207-145.ap-southeast-2.compute.amazonaws.com/',
+  apiBaseUrl: 'https://ec2-13-210-207-145.ap-southeast-2.compute.amazonaws.com',
 };
